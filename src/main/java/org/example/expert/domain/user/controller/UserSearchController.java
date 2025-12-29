ed8f5ab02aaab830a89956a533da3017bee5f525
d-perado <sjm01065@naver.com>
2025-12-29 15:03:11 +0900

    13 레디스 접근 완료

--- src/main/java/org/example/expert/domain/user/controller/UserSearchController.java
+++ src/main/java/org/example/expert/domain/user/controller/UserSearchController.java
@@ -1,8 +1,11 @@
 package org.example.expert.domain.user.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.Value;
 import org.example.expert.domain.user.dto.response.UserResponse;
 import org.example.expert.domain.user.service.UserSearchService;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,9 +17,12 @@ public class UserSearchController {
 
     private final UserSearchService userSearchService;
 
+
+
     @GetMapping("/search/user")
     public ResponseEntity<UserResponse> search(@RequestParam String nickname){
         UserResponse result = userSearchService.searchUser(nickname);
+
         return ResponseEntity.ok(result);
     }
 }
--- src/main/java/org/example/expert/domain/user/service/UserSearchService.java
+++ src/main/java/org/example/expert/domain/user/service/UserSearchService.java
@@ -1,28 +1,47 @@
 package org.example.expert.domain.user.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.example.expert.domain.user.dto.response.UserResponse;
 import org.example.expert.domain.user.entity.User;
 import org.example.expert.domain.user.repository.UserRepository;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
 import java.util.Optional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserSearchService {
 
     private final UserRepository userRepository;
+    private final RedisTemplate<String, UserResponse> redisTemplate;
+    private static final String key = "user:search:";
 
-    public UserResponse searchUser(String nickname){
+    public UserResponse searchUser(String nickname) {
+
+        UserResponse cached = redisTemplate.opsForValue().get(key + nickname);
+
+        if (cached != null) {
+            log.info("레디스 접근");
+            return cached;
+        }
+
         Optional<User> user = userRepository.findByNicknameEquals(nickname);
 
-        if(user.isEmpty()){
-            return null;
+        if (user.isEmpty()) {
+            throw new IllegalArgumentException("유저 존재 x");
         }
 
         User findedUser = user.get();
 
-        return new UserResponse(findedUser.getId(), findedUser.getEmail());
+        UserResponse response = new UserResponse(findedUser.getId(), findedUser.getEmail());
+
+        redisTemplate.opsForValue().set(key + nickname, response, Duration.ofMinutes(5));
+
+        return response;
+
     }
 }
